e28ffa7c7fe1cb3e443abd7c7ec2b1b6a4c10024
avizmzm49 <59847752+avizmzm49@users.noreply.github.com>
2021-01-18 13:35:28 +0200

    add files

---
+++ q1.js
@@ -0,0 +1,19 @@
+//the program accept tow numbers, and display thr larger
+
+let num1 = Number(prompt("enter the first number"));
+let num2 = Number(prompt("Enter the seconde number"));
+
+
+if (num1 > num1) {
+  alert("the larger number is " + num1);
+} else if (num1 < num2) {
+  alert("the larger number is " + num2);
+} else {
+  alert("The numbers equals");
+}
+
+// do {
+//     num1 = prompt("enter the first number");
+//     num2 = prompt("Enter the seconde number");
+// }
+// while (!(typeof (num1) === Number && typeof (num2) === Number));
---
+++ q2.js
@@ -0,0 +1,15 @@
+
+
+
+let x = Number(prompt("enter the first number"));
+let y = Number(prompt("Enter the seconde number"));
+let z = Number(prompt("Enter the three number"));
+
+
+
+if ((z * x * y) < 0) {
+    alert('the sing is -')
+} else {
+    alert("the sing is +");    
+}
+

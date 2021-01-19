2feef4ae18a30ea96e22640d478fe61dcb261233
avizmzm49 <59847752+avizmzm49@users.noreply.github.com>
2021-01-19 11:49:23 +0200

    Exercis 8

--- q8.js
+++ q8.js
@@ -1,15 +1,33 @@
-let num = 11;
-counter = 100;
-let sum = 0;
-let temp = num;
+for (let i = 0; i < 100; i++) {
+  const num = i;
+  let sum = 0;
+  let temp = num;
+  let history = [];
+  while (temp !== 1 && !history.includes(temp)) {
+    history.push(sum);
+    sum = 0;
+    while (temp > 0) {
+      let digit = temp % 10;
+      sum += digit * digit;
+      temp = Math.floor(temp / 10);
+    }
+    temp = sum;
+  }
 
-while (sum != 1 && counter > 0) {
-  temp = sum;
+  if (temp === 1) {
+    console.log(num + " is happy number");
+  } else {
+    // console.log(num + " isn't happy number");
+  }
+}
+
+function sumqubit(num1) {
+  let temp = num1;
+  let sum = 0;
   while (temp > 0) {
     let digit = temp % 10;
-    sum += digit * digit;
+    sum += digit ** 2;
     temp = Math.floor(temp / 10);
-    console.log(temp, sum);
   }
-  counter--;
+  return sum;
 }
